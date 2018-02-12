c334868cead3edbb1258b3e2fd6e2e91635d63ae
Raman <bandariraman@gmail.com>
2018-02-12 17:52:34 +0530

    inital routes

--- server/server.js
+++ server/server.js
@@ -8,38 +8,16 @@ const {Users}=require('./model/Users');
 
 var app=express();
 
-//app.use(bodyParser.json());
+app.get('/properties',(req,res)=>{
+   res.send('show properties');
+});
 
-// app.post('/todos',(req,res)=>{
-//    //console.log(req.body);
-//    var todo=new Todos({
-//        text:req.body.text
-//    });
-//    todo.save().then((doc)=>{
-//        //console.log('Todo created'+doc);
-//        res.status(400).send(doc)
-//    },(err)=>{
-//       // console.log('Error in creating Todo:'+err);
-//       res.send(err);
-//    });
-
-// });
+app.post('/properties',(req,res)=>{
+  res.send('post properties');
+});
 
 
-// app.get('/todos',(req,res)=>{
-    
-//     Todos.find().then((todos)=>{
-//     res.send({
-//         todos
-//     });
-//    });
-// },(err)=>{
-//     console.log('unable to get'+err);
-// })
-
-
-
-var server=app.listen(3007,()=>{
+var server=app.listen(3008,()=>{
    console.log('Started on  port 3000');   
 });
 //Error log file below
